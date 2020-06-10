d054db85815f15695adf345c7a3fc16bfcde10d5
India Rex <face940@gmail.com>
2020-06-10 10:47:51 +0100

    fixes first error

--- debugging/cipher.rb
+++ debugging/cipher.rb
@@ -1,13 +1,9 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
-    p cipher.find_index(char)
-    p cipher
-    p char
     (65 + cipher.find_index(char)).chr
   end
-  p ciphertext_chars
-  p ciphertext_chars.join
+  ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
@@ -15,14 +11,14 @@ def decode(ciphertext, key)
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
-  p plaintext_chars.join
+  plaintext_chars.join
 end
 
 
 # Intended output:
 #
-encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 #
-decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
