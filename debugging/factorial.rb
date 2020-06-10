e5124b21d4f4798e969008fc958dc62fb3188128
India Rex <face940@gmail.com>
2020-06-10 11:30:37 +0100

    cleans fixed code

--- debugging/factorial.rb
+++ debugging/factorial.rb
@@ -1,11 +1,8 @@
 def factorial(n)
-  product = n #changed to n, because factorial is times itself
-  p "at the start product is #{product}"
-  while n > 1 #changed to 1, so that final stage != 0
+  product = n
+  while n > 1
     n -= 1
-    p "we multiply #{product} by #{n}"
     product *= n
-    p "we get #{product}"
   end
   product
 end
