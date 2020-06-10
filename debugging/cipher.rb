7e6a4f0db5f9705c5b8892d17b08f91196932106
India Rex <face940@gmail.com>
2020-06-10 10:45:03 +0100

    debug notes and fix

--- debugging/cipher.rb
+++ debugging/cipher.rb
@@ -1,9 +1,13 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
+    p cipher.find_index(char)
+    p cipher
+    p char
     (65 + cipher.find_index(char)).chr
   end
-  ciphertext_chars.join
+  p ciphertext_chars
+  p ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
@@ -11,13 +15,14 @@ def decode(ciphertext, key)
   plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
-  plaintext_chars.join
+  p plaintext_chars.join
 end
+
 
 # Intended output:
 #
-# > encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 #
-# > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
